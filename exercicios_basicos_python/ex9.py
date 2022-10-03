d4c9e125aa220f5861247c37a835c6674f103d52
Tatiana Maria Demarco <tatimariademarco@gamil.com>
2022-10-03 12:35:15 -0300

    Commit reajuste

--- exercicios_basicos_python/ex9.py
+++ exercicios_basicos_python/ex9.py
@@ -4,6 +4,4 @@ for i in range(0,15):
     valor = input("Insira um valor: ")
     lista_valores.insert(i,valor)
 
-lista_valores2 = list(range(15, 0, -1))
-
-print(lista_valores2)
+print(*lista_valores[::-1])
